3b5546290e2fe563de70dfb9d80c28c4e5910604
David Ogden <DavidOgden112@gmail.com>
2025-08-16 17:23:05 +0100

    refactor: :construction_worker: Changed config files and created docs folder.
    Sorted prettier, eslint and postcss configs.

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,17 @@
-const config = {
-  plugins: ["@tailwindcss/postcss"],
-};
+import tailwindcss from '@tailwindcss/postcss'
+import autoprefixer from 'autoprefixer'
+import cssnano from 'cssnano'
+import postcssImport from 'postcss-import'
 
-export default config;
+const config = {
+  plugins: [
+    postcssImport,
+    tailwindcss,
+    autoprefixer,
+    cssnano({
+      preset: 'default',
+    }),
+  ],
+}
+
+export default config
